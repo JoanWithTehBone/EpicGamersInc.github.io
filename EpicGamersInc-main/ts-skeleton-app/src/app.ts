e485153d0b8f296821ff4697c8669423a937fbcd
JoanWithTehBone <70522505+JoanWithTehBone@users.noreply.github.com>
2021-01-18 09:51:57 +0100

    Delete EpicGamersInc-main/ts-skeleton-app directory

--- EpicGamersInc-main/ts-skeleton-app/src/app.ts
+++
@@ -1,5 +0,0 @@
-console.log("Javascript is working!");
-// Add EventListener to load the game whenever the browser is ready
-window.addEventListener('load', () => {console.log("Handling the Load event");
-//Create a new game to start looping through
-const game = new Game(document.getElementById('canvas'));});
